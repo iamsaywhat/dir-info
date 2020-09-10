716fe0226d28916e5e86e1e300c408a1de3ea082
Andrei Seliverstov <andrei.seliverstov@emlid.com>
2020-09-10 17:36:30 +0300

    dir-info2: add a check for script arguments
    Previously the script didn't check validity of arguments. Now if
    misused, it will thow an error.

--- dir-info2.sh
+++ dir-info2.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
-
-FILENUM=$(ls -a | awk '{print $1}' | wc -l)
 DIR=$1
-FILELIST=$(ls -a | awk '{print $1}')
-for name in $FILELIST; do
-    echo $name" - "$(file $DIR/$name | awk -F": " '{print $2}')"\n"  
-done
-echo "#####################"
-echo "TOTAL FILES: $FILENUM"
+if [ $# -ne 1 ]; then
+    echo "dir-info2: wrong number of arguments!"
+    echo "use dir-info2.sh [path-to-dir]!"
+elif [ ! -d $DIR ]; then
+    echo "script argument isn't directory!"
+else
+    FILENUM=$(ls -a | awk '{print $1}' | wc -l)
+    FILELIST=$(ls -a | awk '{print $1}')
+    for name in $FILELIST; do
+        echo $name" - "$(file $DIR/$name | awk -F": " '{print $2}')"\n"  
+    done
+    echo "#####################"
+    echo "TOTAL FILES: $FILENUM"
+fi

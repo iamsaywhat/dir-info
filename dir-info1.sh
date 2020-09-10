c3ea03797be9e19b2ebe000d708e158813673dcf
Andrei Seliverstov <andrei.seliverstov@emlid.com>
2020-09-10 19:06:24 +0300

    dir-info1: fix for filename with space
    Fix for the script to work with files containing a space in the name.

--- dir-info1.sh
+++ dir-info1.sh
@@ -1,28 +1,29 @@
 #!/bin/sh
 DIR=$1
+echo "$DIR"
 if [ $# -ne 1 ]; then
     echo dir-info1.sh: wrong nubmber of arguments!
     echo use dir-info1.sh [path-to dir]!
-elif [ ! -d $DIR ]; then
+elif [ ! -d "$DIR" ]; then
     echo "script argument isn't directory!"
 else
-    for something in $DIR/*; do
-        if [ -f $something ]; then
-            echo $(basename $something)  - is a file
-         elif [ -h $something ]; then
-             echo $(basename $something)  - is a symlink
-        elif [ -d $something ]; then
-            echo $(basename $something)  - is a directory
-        elif [ -b $something ]; then
-            echo $(basename $something)  - is a block device
-        elif [ -c $something ]; then
-            echo $(basename $something)  - is a character device
-        elif [ -p $something ]; then
-            echo $(basename $something)  - is a named pipe
-        elif [ -S $something ]; then
-            echo $(basename $something)  - is a socket
+    for something in "$DIR"/*; do
+        if [ -f "$something" ]; then
+            echo $(basename "$something")  - is a file
+         elif [ -h "$something" ]; then
+             echo $(basename "$something")  - is a symlink
+        elif [ -d "$something" ]; then
+            echo $(basename "$something")  - is a directory
+        elif [ -b "$something" ]; then
+            echo $(basename "$something")  - is a block device
+        elif [ -c "$something" ]; then
+            echo $(basename "$something")  - is a character device
+        elif [ -p "$something" ]; then
+            echo $(basename "$something")  - is a named pipe
+        elif [ -S "$something" ]; then
+            echo $(basename "$something")  - is a socket
         else
-            echo $(basename $something)  - is unknown 
+            echo $(basename "$something")  - is unknown 
         fi
     done
 fi 
